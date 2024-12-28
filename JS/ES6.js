8fb42dcc815622f75ae54c9be129e54a321adf13
mathav-ramalingam <mathavramalingam1608@gmail.com>
2024-12-28 15:53:21 +0530

    Day 2 : ES6 (Rest opertator etc.....)

--- JS/ES6.js
+++ JS/ES6.js
@@ -1,66 +1,80 @@
-
-
 //function
-function fun(alldep)
-{
-    console.log(alldep)
+function fun(alldep) {
+  console.log(alldep);
 }
 
-fun(["Cse","Cse","Cse"])
-
-
+fun(["Cse", "Cse", "Cse"]);
 
 //arrow function
-arrowfun = (dep) =>{
-    console.log(dep[0])
-}
+arrowfun = (dep) => {
+  console.log(dep[0]);
+};
 
-arrowfun(["cse","It"])
-
+arrowfun(["cse", "It"]);
 
 //desctructing
-console.log("-----------------desctructing-------------")
-mark = [30 , 45 , 46]
-const[m1,m2]=mark
-console.log(m1,m2)
-
-
-
-
+console.log("-----------------desctructing-------------");
+mark = [30, 45, 46];
+const [m1, m2] = mark;
+console.log(m1, m2);
 
 //spread operator (...)
-console.log("-----------------spread-------------------")
-student = ["mathav","maha","Jayanth","Arvi","dinesh"]
-new_s = ["ajay","rajan","anand"]
-console.log(...student,...new_s)
-console.log([...student,...new_s])
-
-
+console.log("-----------------spread-------------------");
+student = ["mathav", "maha", "Jayanth", "Arvi", "dinesh"];
+new_s = ["ajay", "rajan", "anand"];
+console.log(...student, ...new_s);
+console.log([...student, ...new_s]);
 
 //rest operator
-console.log("---------------------------------Rest--------------------------------")
-const [a,b,...c] = student
-console.log(a)
-console.log(b)
-console.log(c)
+console.log(
+  "---------------------------------Rest--------------------------------"
+);
+const [a, b, ...c] = student;
+console.log(a);
+console.log(b);
+console.log(c);
 
-function restop(...foods)
-{
-    console.log(foods)
+function restop(...foods) {
+  console.log(foods);
 }
 
-f1 = "parotta"
-f2 = "Biriyani"
-f3 = "Rice"
-restop(f1,f2,f3)
-
+f1 = "parotta";
+f2 = "Biriyani";
+f3 = "Rice";
+restop(f1, f2, f3);
 
 //scope
-console.log("----------------------scope------------------")
-var v = 7
-console.log(v)
+//let        ==>> block/local scope
+//var/const  ==>> global scope
+
+console.log("----------------------scope ------------------");
+var v = 7;
+console.log(v);
 {
-    let v=40
-    console.log(v)
+  let v = 40;
+  console.log(v);
 }
-console.log(v)
+console.log(v);
+
+console.log("---------------------without let ------------------");
+var v = 100;
+console.log(v);
+{
+  v = 50;
+  console.log(v);
+}
+console.log(v);
+
+//hosting
+console.log("------------------------------------hosting---------------------");
+console.log(z); //undefined
+var z = 10;
+console.log(z);
+
+console.log(y); //reference error
+let y = 10;
+console.log(y);
+
+console.log(x); //reference error
+const x = 10;
+console.log(x);
